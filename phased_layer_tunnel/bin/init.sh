3d5b0193c95fd21e7a0c12684c96f14c84d6dee0
Your Name <cliff.cyphers@gmail.com>
2012-02-12 21:41:03 -0800

    cleaned up init to provide start & stop

--- phased_layer_tunnel/bin/init.sh
+++ phased_layer_tunnel/bin/init.sh
@@ -1,9 +1,28 @@
 #!/bin/sh
+_start_() {
+  export HOME=/phased_layer_tunnel
+  export GEM_HOME=$HOME/gems
+  export PATH=/opt/ruby193/bin:$GEM_HOME/bin:$PATH
+  cd $HOME
+  sudo $HOME/bin/start_purge_agent.sh &
+  echo $! >$HOME/start_purge_agent.pid
+  bundle exec unicorn -c config/unicorn.rb config.ru &
+  echo $! >$HOME/unicorn.pid
+}
 
-export HOME=/phased_layer_tunnel
-export GEM_HOME=$HOME/gems
-export PATH=/opt/ruby193/bin:$GEM_HOME/bin:$PATH
-cd $HOME
-sudo $HOME/bin/start_purge_agent.sh &
-bundle exec unicorn -c config/unicorn.rb config.ru &
+_stop_() {
+  kill  `cat /jail/phasunnel/start_purge_agent.pid /jail/phased_layer_tunnel/unicorn.pid | awk '{printf $1 " "}'`
+  rm -f /jail/phasunnel/start_purge_agent.pid /jail/phased_layer_tunnel/unicorn.pid
+}
 
+
+case "$1" in
+  start)
+    _start_
+    ;;
+  stop)
+    _stop_
+    ;;
+  *)
+    echo 'usaage'
+esac
